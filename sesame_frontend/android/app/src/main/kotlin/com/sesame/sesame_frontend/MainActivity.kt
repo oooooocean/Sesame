9a3b4e858bc320d673fd5f37e08c1cfd41834461
chenqiang <chenqiang19901202@163.com>
2022-03-30 23:08:25 +0800

    add frontend

---
+++ sesame_frontend/android/app/src/main/kotlin/com/sesame/sesame_frontend/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sesame.sesame_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ sesame_frontend/node.md
@@ -0,0 +1,6 @@
+# 工程
+## 1. 创建工程
+
+```shell
+flutter create -i swift -a kotlin --platforms ios,android,web --org com.sesame  sesame_frontend
+```
