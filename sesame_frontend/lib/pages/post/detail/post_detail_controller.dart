88ac8331e9bea2392f20f2d6ca15cdc689bd4ccd
dingwenkai <dingwenkai@huami.com>
2022-05-18 00:24:42 +0800

    feat |> 评论后列表数据刷新

--- sesame_frontend/lib/pages/post/detail/post_detail_controller.dart
+++ sesame_frontend/lib/pages/post/detail/post_detail_controller.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart' show ScrollController, TabController;
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
+import 'package:sesame_frontend/components/utils/event_bust_util.dart';
 import 'package:sesame_frontend/models/paging_data.dart';
 import 'package:sesame_frontend/models/post.dart';
 import 'package:sesame_frontend/models/post_comment.dart';
@@ -10,6 +11,8 @@ import 'package:sesame_frontend/net/net_mixin.dart';
 import 'package:sesame_frontend/pages/post/detail/post_comment_edit_page.dart';
 import 'package:sesame_frontend/pages/post/views/post_handler_tile.dart';
 import 'package:sesame_frontend/route/pages.dart';
+
+import 'package:sesame_frontend/components/utils/event_bust_util.dart';
 
 class PostDetailController extends GetxController with NetMixin {
   final Post data;
@@ -67,6 +70,7 @@ class PostDetailController extends GetxController with NetMixin {
       comments.insert(0, comment);
       update();
     });
+    EventBusUtil.getInstance().fire(PageAction(true));
   }
 
   void load(RefreshType refreshType) {
--- sesame_frontend/lib/pages/post/list/post_list_controller.dart
+++ sesame_frontend/lib/pages/post/list/post_list_controller.dart
@@ -1,16 +1,40 @@
+import 'dart:ffi';
+
 import 'package:get/get.dart';
 import 'package:sesame_frontend/components/mixins/refresh_mixin.dart';
+import 'package:sesame_frontend/components/utils/event_bust_util.dart';
 import 'package:sesame_frontend/models/paging_data.dart';
 import 'package:sesame_frontend/models/post.dart';
 import 'package:sesame_frontend/net/net_mixin.dart';
 import 'package:sesame_frontend/route/pages.dart';
+import 'package:sesame_frontend/components/utils/event_bust_util.dart';
 
-class PostListController extends GetxController with NetMixin, RefreshMixin<Post> {
+class PostListController extends GetxController
+    with NetMixin, RefreshMixin<Post> {
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+    EventBusUtil.getInstance().on<PageAction>().listen((event) {
+      load(RefreshType.refresh);
+    });
+  }
+
+  @override
+  void onClose() {
+    // TODO: implement onClose
+    super.onClose();
+    EventBusUtil.getInstance().destroy();
+  }
+
   void onTapAvatar(Post post) {}
 
   void onTapPhoto(Post post, int photoIndex) {
-    Get.toNamed(AppRoutes.photoBrowser,
-        arguments: [post.photos, photoIndex, (photo) => post.id.toString() + photo.name]);
+    Get.toNamed(AppRoutes.photoBrowser, arguments: [
+      post.photos,
+      photoIndex,
+      (photo) => post.id.toString() + photo.name
+    ]);
   }
 
   void onTap(Post post) {
@@ -22,6 +46,8 @@ class PostListController extends GetxController with NetMixin, RefreshMixin<Post
   }
 
   @override
-  Future<PagingData<Post>> get refreshRequest => get('posts/', {'page': paging.current.toString()},
+  Future<PagingData<Post>> get refreshRequest => get(
+      'posts/',
+      {'page': paging.current.toString()},
       (data) => PagingData.fromJson(data, (json) => Post.fromJson(json)));
 }
